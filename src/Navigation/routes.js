fd0ce239b9f13e40011e5df680461153923a1f1b
M-Hasssan <muhammed.hassan.tariq@gmail.com>
2024-09-27 20:40:33 +0500

    Added Contact

--- src/Navigation/routes.js
+++ src/Navigation/routes.js
@@ -1,12 +1,14 @@
 import HomeScreen from "../screens/Dashboard/Home";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
+import ContactScreen from "../screens/Dashboard/Contact";
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen  name="Contact Support" component={ContactScreen} />
+
     </Stack.Navigator>
   );
 }
---
+++ src/screens/Dashboard/Contact.js
@@ -0,0 +1,118 @@
+import {
+  View,
+  StyleSheet,
+  Image,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import { colors } from "../../utils/colors";
+import React, { useState } from "react";
+
+function ContactScreen() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  // Button Alert
+
+  const handlePress = () => {
+    alert("Your Message has been submitted");
+  };
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <View style={styles.contain}>
+        <Image
+          source={require("../../../assets/Contact.png")}
+          style={styles.image}
+        />
+
+        <Text style={styles.label}>Name</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your name"
+          value={name}
+          onChangeText={setName}
+        />
+
+        <Text style={styles.label}>E-mail</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Enter your e-mail"
+          value={email}
+          onChangeText={setEmail}
+        />
+
+        <Text style={styles.label}>Message</Text>
+
+        <TextInput
+          style={[styles.input, { height: 80 }]}
+          placeholder="Enter a message"
+          multiline={true}
+          numberOfLines={4}
+          value={message}
+          onChangeText={setMessage}
+        />
+
+        <TouchableOpacity style={styles.button} onPress={handlePress}>
+          <Text style={styles.buttonText}>Send</Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  contain: {
+    flex: 1,
+    flexGrow: 1, // Allows the ScrollView to grow with the content
+    padding: 30,
+    alignItems: "center",
+    backgroundColor: colors.white,
+  },
+
+  image: {
+    marginTop: 20,
+    marginBottom: 10,
+    width: 300,
+    height: 200,
+  },
+
+  label: {
+    fontSize: 15,
+    width: 280,
+    color: "gray",
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 10,
+    width: 280,
+    marginBottom: 20,
+    paddingLeft: 10,
+  },
+  output: {
+    fontSize: 18,
+  },
+
+  button: {
+    backgroundColor: colors.primary,
+    paddingHorizontal: 122,
+    padding: 15,
+    marginTop: 10,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    textAlign: "center",
+  },
+});
+
+export default ContactScreen;
