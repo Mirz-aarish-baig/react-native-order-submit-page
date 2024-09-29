927a17c2d3cdcb2a1aed06545328dc5cb6c17250
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-09-29 23:25:07 +0500

    splashscreen-admin

--- src/Navigation/routes.js
+++ src/Navigation/routes.js
@@ -1,12 +1,15 @@
 import HomeScreen from "../screens/Dashboard/Home";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import splashscreen from "../screens/admin-panel/splashscreen";
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} />
+<Stack.Screen name="splashscreen" component={splashscreen} />  
+
+      {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
     </Stack.Navigator>
   );
 }
